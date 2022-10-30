12a284023abb0f136986d0fc007de804e08849b5
milankomaj <milankomaj+Github@gmail.com>
2022-10-30 03:02:41 +0100

    git fetch

--- clean_relases.sh
+++ clean_relases.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 gh release list
 gh release list --limit 9999 | gawk -F '\t' '$1 == "v0.2" {print $3}' | xargs -L1 gh release delete --yes
+git fetch
 git tag -l | xargs -n 1 git push --delete origin
